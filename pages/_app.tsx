4be7c2cb594aae4b44299357d15e0c5fa8caf44b
alan <alanarostegui@hotmail.com>
2022-08-10 00:09:36 -0500

    feat: add baseUrl to ts config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { lightTheme } from "../themes";
+import { lightTheme } from "themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
