d4fe6c5ce5526754736d3459c1d92433cc8bcdee
alan <alanarostegui@hotmail.com>
2022-08-18 00:10:09 -0500

    feat: set initialData in ProductList with a ProductCard

---
+++ components/products/ProductList.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { Grid } from "@mui/material";
+import { IProduct } from "interfaces";
+import { ProductCard } from "./";
+
+interface Props {
+  products: IProduct[];
+}
+export const ProductList: FC<Props> = ({ products }) => {
+  return (
+    <Grid container spacing={4}>
+      {products.map((product) => (
+        <ProductCard product={product} key={product.slug} />
+      ))}
+    </Grid>
+  );
+};
