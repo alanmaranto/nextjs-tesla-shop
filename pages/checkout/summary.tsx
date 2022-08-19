27f6a4cdd1ec732daa6b353672b7c78393b9928c
alan <alanarostegui@hotmail.com>
2022-08-19 14:38:34 -0500

    feat: summary order page

---
+++ pages/checkout/summary.tsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Link,
+  Typography,
+} from "@mui/material";
+import { CartList, OrderSummary } from "components/cart";
+import { ShopLayout } from "../../components/layouts/ShopLayout";
+import NextLink from "next/link";
+
+const SummaryPage = () => {
+  return (
+    <ShopLayout title="Orden summary" pageDescription="Order summary">
+      <Typography variant="h1" component="h1">
+        Order summary
+      </Typography>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CartList />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card className="summary-card">
+            <CardContent>
+              <Typography variant="h2">Summary (3 products) </Typography>
+              <Divider sx={{ my: 1 }} />
+              <Box display="flex" justifyContent="space-between">
+              <Typography variant="subtitle1">Shipping address</Typography>
+                <NextLink href="/checkout/address" passHref>
+                  <Link underline="always">Edit</Link>
+                </NextLink>
+              </Box>
+              <Typography>Alan Maranto</Typography>
+              <Typography>453 Always Blue</Typography>
+              <Typography>London 283</Typography>
+              <Typography>England</Typography>
+              <Typography>+52 558193930</Typography>
+              <Divider sx={{ my: 1 }} />
+              <Box display="flex" justifyContent="end">
+                <NextLink href="/cart" passHref>
+                  <Link underline="always">Edit</Link>
+                </NextLink>
+              </Box>
+              <OrderSummary />
+              <Box sx={{ mt: 3 }}>
+                <Button color="secondary" className="circular-btn" fullWidth>
+                  Confirm Order
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default SummaryPage;
