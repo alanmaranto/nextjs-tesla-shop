f4007f031a7cc7aedef3c1b37de4c414e464408b
alan <alanarostegui@hotmail.com>
2022-08-19 00:42:18 -0500

    feat: show cart products

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,5 +1,15 @@
-import { Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Link,
+  Typography,
+} from "@mui/material";
+import { ItemCounter } from "components/ui";
 import { initialData } from "database/products";
+import NextLink from "next/link";
 
 const productsInCart = [
   initialData.products[0],
@@ -11,7 +21,47 @@ export const CartList = () => {
   return (
     <>
       {productsInCart.map((product) => (
-        <Typography key={product.slug}>{product.slug}</Typography>
+        <Grid container spacing={2} key={product.slug} sx={{ mb: 1 }}>
+          <Grid item xs={3}>
+            {/* TODO: Go to product page */}
+            <NextLink href="/product/slug" passHref>
+              <Link>
+                <CardActionArea>
+                  <CardMedia
+                    image={`products/${product.images[0]}`}
+                    component="img"
+                    sx={{ borderRadius: "5px" }}
+                  />
+                </CardActionArea>
+              </Link>
+            </NextLink>
+          </Grid>
+          <Grid item xs={7}>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="body1">{product.title}</Typography>
+              <Typography variant="body1">
+                Size: <strong>M</strong>
+              </Typography>
+              {/* Conditional Item Counter TODO:  */}
+              <ItemCounter />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="subtitle1">${product.price}</Typography>
+            {/* TODO: Editable */}
+            <Button variant="text" color="secondary">
+              Remove
+            </Button>
+          </Grid>
+        </Grid>
+
+        // <Typography key={product.slug}>{product.title}</Typography>
       ))}
     </>
   );
