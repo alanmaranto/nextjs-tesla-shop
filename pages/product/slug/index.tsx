5bcd408649fbf9a1d1606a79d8245038ec2b8922
alan <alanarostegui@hotmail.com>
2022-08-18 23:01:22 -0500

    feat: select size on product slug

--- pages/product/slug/index.tsx
+++ pages/product/slug/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import { ShopLayout } from "components/layouts";
+import { SizeSelector } from "components/products";
 import { ItemCounter } from "components/ui";
 import { initialData } from "database/products";
 import React from "react";
@@ -23,11 +24,13 @@ const ProductPage = () => {
             <Typography variant="subtitle1" component="h2">
               ${product.price}
             </Typography>
-
-            {/* Quantity */}
             <Box sx={{ my: 2 }}>
               <Typography variant="subtitle2">Quantity</Typography>
               <ItemCounter />
+              <SizeSelector
+                // selectedSize={product.sizes[0]}
+                sizes={product.sizes}
+              />
             </Box>
             {/* Add to cart */}
             <Button color="secondary" className="circular-btn">
