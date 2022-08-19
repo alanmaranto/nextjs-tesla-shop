6deca0bab6ffe0ad16d2c174ec6d9ae1bdde8121
alan <alanarostegui@hotmail.com>
2022-08-19 16:30:22 -0500

    feat: add register page

---
+++ pages/auth/register.tsx
@@ -0,0 +1,51 @@
+import { Box, Button, Grid, TextField, Typography, Link } from "@mui/material";
+import { AuthLayout } from "components/layouts";
+import React from "react";
+import NextLink from "next/link";
+
+const RegisterPage = () => {
+  return (
+    <AuthLayout title="Sign in">
+      <Box sx={{ width: 350, padding: "10px 20px" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h1" component="h1">
+              Create account
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Fullname" variant="filled" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Email" variant="filled" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Password"
+              type="password"
+              variant="filled"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              color="secondary"
+              className="circular-btn"
+              size="large"
+              fullWidth
+            >
+              Sign in
+            </Button>
+          </Grid>
+          <Grid item xs={12} display="flex" justifyContent="end">
+            <NextLink href="/auth/login" passHref>
+              <Link underline="always">Already have an account? Sign in</Link>
+            </NextLink>
+          </Grid>
+        </Grid>
+      </Box>
+    </AuthLayout>
+  );
+};
+
+export default RegisterPage;
