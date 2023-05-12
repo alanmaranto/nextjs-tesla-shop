672731012e634d1bafef1936594f543dfe9f8db0
alanmaranto <alanarostegui@hotmail.com>
2023-05-12 00:37:16 -0600

    feat: add mongoose and product model

---
+++ models/Product.ts
@@ -0,0 +1,44 @@
+import { IProduct } from "interfaces";
+import { Schema, model, Model, models } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    images: [{ type: String }],
+    inStock: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    sizes: [
+      {
+        type: String,
+        enum: {
+          values: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+          message: "{VALUE} is not a valid size",
+        },
+      },
+    ],
+    slug: { type: String, required: true, unique: true },
+    tags: [{ type: String }],
+    title: { type: String, required: true },
+    type: {
+      type: String,
+      enum: {
+        values: ["shirts", "pants", "hoodies", "hats"],
+        message: "{VALUE} is not a valid type",
+      },
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["men", "women", "kid", "unisex"],
+        message: "{VALUE} is not a valid genre",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+//TODO: Create mongo index to search between columns
+const Product: Model<IProduct> =
+  models.Product || model("Product", productSchema);
+
+export default Product;
