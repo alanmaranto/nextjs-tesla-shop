3bcb9a56a8a7c88458ae19d5871a17253e3218e0
alanmaranto <alanarostegui@hotmail.com>
2023-05-16 20:24:04 -0600

    feat: get product by slug

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,40 @@
+import { IProduct } from "interfaces";
+import { db } from "database";
+import Product from "models/Product";
+import { NextApiRequest, NextApiResponse } from "next";
+
+type Data = { msg: string } | IProduct;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "GET":
+      return getProductsBySlug(req, res);
+
+    default:
+      return res.status(400).json({ msg: "Bad Request" });
+  }
+}
+
+async function getProductsBySlug(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await db.connect();
+
+  const { slug } = req.query;
+
+  const product = await Product.findOne({ slug }).lean();
+
+  await db.disconnect();
+
+  if (!product) {
+    return res.status(404).json({
+      msg: "Product not found",
+    });
+  }
+
+  return res.json(product);
+}
