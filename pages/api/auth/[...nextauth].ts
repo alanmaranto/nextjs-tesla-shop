a8ad808be5b1043d78a50977b3d8699834f645fd
alanmaranto <alanarostegui@hotmail.com>
2023-08-01 18:58:35 -0600

    feat: next auth providers

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,26 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
+    Credentials({
+      name: 'Custom login',
+      credentials: {
+        email: { label: 'Email:', type: 'email', placeholder: "email@google.com" },
+        password: { label: 'Password:', type: 'password', placeholder: "password" },
+      },
+      async authorize(credentials) {
+        return null
+      }
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
+  callbacks: {
+    
+  }
 });
