33953773d475ea31e014d8d3c105c0c7fd03de49
alanmaranto <alanarostegui@hotmail.com>
2023-08-01 19:41:09 -0600

    feat: nextauth callbacks

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,14 +6,22 @@ export default NextAuth({
   providers: [
     // OAuth authentication providers...
     Credentials({
-      name: 'Custom login',
+      name: "Custom login",
       credentials: {
-        email: { label: 'Email:', type: 'email', placeholder: "email@google.com" },
-        password: { label: 'Password:', type: 'password', placeholder: "password" },
+        email: {
+          label: "Email:",
+          type: "email",
+          placeholder: "email@google.com",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "password",
+        },
       },
       async authorize(credentials) {
-        return null
-      }
+        return null;
+      },
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -21,6 +29,25 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    
-  }
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+
+        switch (account.type) {
+          case "oauth":
+            // TODO: create of verify is user exists in db
+            break;
+          case "credentials":
+            token.user = user;
+            break;
+        }
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken as any;
+      session.user = token.user as any;
+      return session;
+    },
+  },
 });
