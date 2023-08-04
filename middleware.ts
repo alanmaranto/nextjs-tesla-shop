890999a35e99e029f680be4e25c90918157e0601
alanmaranto <alanarostegui@hotmail.com>
2023-08-03 23:41:17 -0600

    feat: update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,22 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-import { jwtVerify } from "jose";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  const previousPage = req.nextUrl.pathname;
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  if (previousPage.startsWith("/checkout")) {
-    const token = req.cookies.get("token")?.value;
-    if (!token) {
-      return NextResponse.redirect(
-        new URL(`/auth/login?p=${previousPage}`, req.url)
-      );
-    }
-    try {
-      await jwtVerify(
-        token,
-        new TextEncoder().encode(process.env.JWT_SECRET_SEED)
-      );
-      return NextResponse.next();
-    } catch (error) {
-        console.log(error);
-      return NextResponse.redirect(
-        new URL(`/auth/login?p=${previousPage}`, req.url)
-      );
-    }
+  if (!session) {
+    const requestedPage = req.nextUrl.pathname;
+    const url = req.nextUrl.clone();
+    url.pathname = `/auth/login`;
+    url.search = `p=${requestedPage}`;
+    return NextResponse.redirect(url);
   }
+
+  return NextResponse.next();
 }
 
+// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/checkout/:path*"],
+  matcher: ["/checkout/address", "/checkout/summary"],
 };
