a7e126346fb58f6ac41e146ffc417a038606a702
alanmaranto <alanarostegui@hotmail.com>
2023-08-03 22:40:17 -0600

    feat: register sign in with next auth

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -16,6 +16,8 @@ import { ErrorOutline } from "@mui/icons-material";
 import { validations } from "utils";
 import { AuthContext } from "context";
 import { useRouter } from "next/router";
+import { getSession, signIn } from "next-auth/react";
+import { GetServerSideProps } from "next";
 
 type FormData = {
   email: string;
@@ -50,8 +52,9 @@ const RegisterPage = () => {
       return;
     }
 
-    const destination = router.query.p?.toString() || "/";
-    router.replace(destination);
+    await signIn("credentials", { email, password });
+    // const destination = router.query.p?.toString() || "/";
+    // router.replace(destination);
   };
   return (
     <AuthLayout title="Sign in">
@@ -140,6 +143,27 @@ const RegisterPage = () => {
       </form>
     </AuthLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  query,
+}) => {
+  const session = await getSession({ req });
+  const { p = "/" } = query;
+
+  if (session) {
+    return {
+      redirect: {
+        destination: p.toString(),
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default RegisterPage;
